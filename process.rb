b8cef24b8b53d8591b3887e6b1adcbadb354ca05
zunda <zundan@gmail.com>
2026-06-12 15:42:16 -1000

    Update usage to generate an .ics file

--- process.rb
+++ process.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-# Prints holidays listed in PDF files linked from a web page. Usage:
+# Generates ics file for holidays listed in PDF files linked from a web page.
+#
+# Usage:
 #
 # 1. Run `bnudle install`
-# 1. Run `bundle exec ruby process.rb https://dhrd.hawaii.gov/state-observed-holidays/`
+# 1. Run `bundle exec ruby process.rb https://dhrd.hawaii.gov/state-observed-holidays/ > holidays.ics`
+# 1. Import `holidays.ics` to your calendar
 #
 require 'icalendar'
 require 'nokogiri'
